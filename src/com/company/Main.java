2301964d78c241c382d0e8e118fc274cf023e512
laoqiu <1203254926@qq.com>
2021-06-06 16:45:23 +0800

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2240,6 +2240,45 @@ public class Main {
         return top / bot;
     }
 
+    /**
+     * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+     *
+     * 请你找出并返回 strs 的最大子集的大小，该子集中 最多 有 m 个 0 和 n 个 1 。
+     *
+     * 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/ones-and-zeroes
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param strs
+     * @param m
+     * @param n
+     * @return
+     */
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        int length = strs.length;
+        for (int i = 0; i < length; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = m; j >= zeros; j--) {
+                for (int k = n; k >= ones; k--) {
+                    dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public int[] getZerosOnes(String str) {
+        int[] zerosOnes = new int[2];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str.charAt(i) - '0']++;
+        }
+        return zerosOnes;
+    }
+
     public static void main(String[] args) {
 
         Main main = new Main();
